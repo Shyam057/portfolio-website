443581c6e101f15e1fd9c71c49274d967e6c6d55
Shyam Kumar Bhattarai <shyambhattarai057@gmail.com>
2026-05-02 17:01:53 -0500

    Add deployment configuration

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # ← add this line
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -143,3 +144,6 @@ CKEDITOR_CONFIGS = {
         'width': '100%',
     },
 }
+# Production static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
